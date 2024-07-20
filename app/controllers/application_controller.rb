988c11126e748ca9e28fd5ca1503e43eb77f5929
EC2 Default User <ec2-user@ip-172-31-19-167.ap-northeast-1.compute.internal>
2024-07-20 17:22:40 +0000

    [Update] 現状を一時保存

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
+#全てのコントローラに対する処理を行える権限を持つController
+
 class ApplicationController < ActionController::Base
+  #devise利用の機能（ユーザー登録やログイン認証など）が使われる前にメソッドを実行
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected   #privateとは違い呼び出された他のコントローラからも参照可能
+
+  #指定したデータを保存できるよう許可を与える
+  def configure_permitted_parameters
+    #sign_up（ユーザ登録）の時にname（ユーザー名）のデータ操作を許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  #アプリを起動した瞬間にtopアクションを実行
+  root to: 'homes#top'
+  get 'home/about', to: "homes#about"
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
