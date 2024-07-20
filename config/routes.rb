21f144dd12d0e002c80488fb8d0ed3404f47c0bb
EC2 Default User <ec2-user@ip-172-31-19-167.ap-northeast-1.compute.internal>
2024-07-20 19:33:39 +0000

    [Update] headerとfooter作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'books/update'
   #アプリを起動した瞬間にtopアクションを実行
   root to: 'homes#top'
+  get '/about', to: 'pages#about'
+
+      #↓消すかも？
   get 'home/about', to: "homes#about"
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
