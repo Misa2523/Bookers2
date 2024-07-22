168aa2b8c0d37bc7c453b9c92ffe1570cbc7028b
EC2 Default User <ec2-user@ip-172-31-19-167.ap-northeast-1.compute.internal>
2024-07-22 08:29:24 +0000

    [Update] 一時保存

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,36 @@
 class UsersController < ApplicationController
 
+  #ログイン直後の表示されるページ
   def show
     #URLに記載されたIDを参考に必要なUserモデルを取得
     @user = User.find(params[:id])
-
   end
-
 
   def index
   end
 
+  #ユーザー情報の編集機能
   def edit
+    #URLを参考に特定のidを持ったレコードを取得
+    @user = User.find(params[:id])
   end
+
+  #更新機能
+  def update
+    #ユーザーの取得
+    @user = User.find(params[:id])
+    #ユーザーのアップデート
+    @user.update(user_params)
+    #ユーザーの詳細ページへ遷移
+    redirect_to user_path(@user.id)
+  end
+
 
   #ストロングパラメータ
   private
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :introduction)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
   #deviseで使用する機能
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   #profile_imageという名前で、ActiveStorageでプロフィール画像を保存できるよう設定
   has_one_attached :profile_image
-  
+
   #画像が投稿されない場合のエラー回避
   #アクションと違い、特定の処理を名前で呼び出す
   def get_profile_image(width, height)
@@ -22,6 +22,6 @@ class User < ApplicationRecord
     profile_image.variant(resize_to_limit: [width, height]).processed
     # ↑このメソッドを実行する際get_profile_image(100, 100)のように引数を設定すると100x100の画像にリサイズされる
   end
-  
-  
+
+
 end
